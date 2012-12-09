1e5f9f9fa79fff72cd3de124adeb4d06ab4d2e7c
David Silver <dsilver@candidatemetrics.com>
2012-12-09 12:24:25 -0800

    setting up

--- example-before/app/controllers/tasks_controller.rb
+++
@@ -1,7 +0,0 @@
-class TasksController < ApplicationController
-  def create
-    @project = Project.find(params[:project_id])
-    @project.tasks.create!(params[:task])
-    redirect_to @project, notice: "Task was successfully added."
-  end
-end
--- example-before/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Example::Application.routes.draw do
-  resources :projects do
-    resources :tasks
-  end
-  root to: 'projects#index'
-end
